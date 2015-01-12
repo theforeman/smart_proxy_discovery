3a52572402d662dd882732329c011db1b8650c6e
Shlomi Zadok <shlomi@ben-hanna.com>
2015-01-12 18:06:13 +0200

    fixes #8907 - pin rest-client to support ruby 1.8.7

--- test/discovery_api_test.rb
+++ test/discovery_api_test.rb
@@ -26,13 +26,12 @@ class DiscoveryApiTest < Test::Unit::TestCase
   def test_create_facts_success
     stub_request(:post, "#{@foreman_url}/api/v2/discovered_hosts/facts")
     post "/create", @facts
-    assert_empty last_response.body
+    assert last_response.body.empty?
     assert last_response.successful?
   end
 
   def test_create_facts_failure
-    stub_request(:post, "#{@foreman_url}/api/v2/discovered_hosts/facts")
-      .to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
+    stub_request(:post, "#{@foreman_url}/api/v2/discovered_hosts/facts").to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
     post "/create", @facts
     assert_equal 500, last_response.status
   end
@@ -40,13 +39,12 @@ class DiscoveryApiTest < Test::Unit::TestCase
   def test_refresh_facts_success
     stub_request(:get, "http://#{@discovered_node}:8443/facts")
     get "/#{@discovered_node}/facts"
-    assert_empty last_response.body
+    assert last_response.body.empty?
     assert last_response.successful?
   end
 
   def test_refresh_facts_failure
-    stub_request(:get, "http://#{@discovered_node}:8443/facts")
-      .to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
+    stub_request(:get, "http://#{@discovered_node}:8443/facts").to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
     get "/#{@discovered_node}/facts"
     assert_equal 500, last_response.status
   end
@@ -54,13 +52,12 @@ class DiscoveryApiTest < Test::Unit::TestCase
   def test_reboot_success
     stub_request(:put, "http://#{@discovered_node}:8443/bmc/#{@discovered_node}/chassis/power/cycle")
     put "/#{@discovered_node}/reboot"
-    assert_empty last_response.body
+    assert last_response.body.empty?
     assert last_response.successful?
   end
 
   def test_reboot_failure
-    stub_request(:put, "http://#{@discovered_node}:8443/bmc/#{@discovered_node}/chassis/power/cycle")
-      .to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
+    stub_request(:put, "http://#{@discovered_node}:8443/bmc/#{@discovered_node}/chassis/power/cycle").to_return(:body => '{"status": "error", "message": "blah"}', :status => 500)
     put "/#{@discovered_node}/reboot"
     assert_equal 500, last_response.status
   end
